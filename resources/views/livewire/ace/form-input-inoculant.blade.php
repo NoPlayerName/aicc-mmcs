ea483bc2cc209833285ff1efe9f4e049d61d9949
Muhammad Ruhiyat <ruhiyat_muhammad@ijtt-id.com>
2026-05-18 14:29:05 +0700

    change alias

--- resources/views/livewire/ace/form-input-inoculant.blade.php
+++ resources/views/livewire/ace/form-input-inoculant.blade.php
@@ -43,7 +43,7 @@
                                         id="product-select2">
                                         <option value="">Choose Product</option>
                                         @foreach ($products as $product)
-                                        <option value="{{ $product['id'] }}">{{ $product['name'] }}</option>
+                                        <option value="{{ $product['id'] }}">{{ $product['alias'] }}</option>
                                         @endforeach
                                     </select>
                                     @error('product')
